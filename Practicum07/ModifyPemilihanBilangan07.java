11b5a5611f557d0b38d12ff252bd872703045480
07nawraa <anindyanauraanindya@gmail.com>
2024-09-27 10:47:25 +0700

    Modify pemilihan angka use terminary

---
+++ Practicum07/ModifyPemilihanBilangan07.java
@@ -0,0 +1,14 @@
+/**
+ * ModifyPemilihanBilangan07
+ */
+import java.util.Scanner;
+public class ModifyPemilihanBilangan07 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Masukkan sebuah angka: ");
+    int angka = sc.nextInt();
+    String hasil;
+    hasil = (angka % 2 == 0) ? "bilangan genap" : "bilangan negatif";
+System.out.println(angka + " adalah " + hasil);
+}
+}

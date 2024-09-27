912b4e6a06a6bed05f2b989369ffd75f41ccc3b2
07nawraa <anindyanauraanindya@gmail.com>
2024-09-27 08:08:04 +0700

    pemilihan hari jobsheet4

---
+++ Practicum07/PemilihanHari07.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class PemilihanHari07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        String dayName, dayType;
+        System.out.print("Input day name: ");
+        dayName = sc.nextLine();
+        switch (dayName.toLowerCase()) {
+            case "monday":
+            case "tuesday":
+            case "wednesday":
+            case "thursday":
+            case "friday":
+                dayType = "weekday";
+                break;
+            case "saturday":
+            case "sunday":
+                dayType = "weekend";
+                break;
+            default:
+                dayType = "invalid day name";
+        } System.out.println(dayName + " is a " + dayType);
+    }
+    }
+

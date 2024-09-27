16557383442bd82de39b0925b57664ebd55cb332
07nawraa <anindyanauraanindya@gmail.com>
2024-09-27 11:49:22 +0700

    Pemilihan hari dengan if

--- Practicum07/PemilihanHariDenganIf07.java
+++ Practicum07/PemilihanHariDenganIf07.java
@@ -1,6 +1,18 @@
-import util.java.Scanner;
+import java.util.Scanner;
 public class PemilihanHariDenganIf07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int dayNumber;
+        String dayType;
+        System.out.print("Input the number of the day: ");
+        dayNumber = sc.nextInt();
+        if (dayNumber <= 5) {
+            System.out.println( dayNumber + " is weekdays");
+        } else if (dayNumber < 8){
+            System.out.println( dayNumber + " is weekend");
+        } else {
+            System.out.println("invalid number");
     }
+}
 }

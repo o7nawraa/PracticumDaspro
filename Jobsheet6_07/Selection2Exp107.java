bc9495594dec03556393b12cf1ab570da399aea0
07nawraa <anindyanauraanindya@gmail.com>
2024-10-04 14:24:43 +0700

    Tahun 2000 apakah kabisat atau tidak

--- Jobsheet6_07/Selection2Exp107.java
+++ Jobsheet6_07/Selection2Exp107.java
@@ -12,7 +12,9 @@ public static void main(String[] args) {
 if (year%4 == 0){
     if (year%100 != 0) { 
         System.out.println("Leap year");
-    } if (year%100 == 0){
+    } if (year%400 == 0){
+        System.out.println("Leap year");
+    } else if (year%100 == 0){
         System.out.println("Not a leap year");
     }
     } else 

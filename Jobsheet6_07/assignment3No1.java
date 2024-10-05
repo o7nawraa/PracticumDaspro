46f3bbf099def1da46d1e4958da75b55f1c3e57d
07nawraa <anindyanauraanindya@gmail.com>
2024-10-05 19:03:57 +0700

    maximum number

--- Jobsheet6_07/assignment3No1.java
+++ Jobsheet6_07/assignment3No1.java
@@ -13,11 +13,11 @@ public class assignment3No1 {
         if (num1 > num2) {
             if (num1 > num3) {
             System.out.print("Maximum number is num 1 : " + num1);
-             } else if(num1 < num3) {
+             } else {
             System.out.print("Maximum number is num 3 : " + num3);
-             } else if (num2 > num3) {
-            System.out.print("Maximum number is num 2 : " + num2);
             } 
+        } else if (num2 > num3) {
+        System.out.print("Maximum number is num 2 : " + num2);
         } else 
             System.out.println("maximum number is num3 : " + num3);
         } 
